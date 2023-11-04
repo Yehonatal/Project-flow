160ae0798d97967549eeaacf185b94e79801ac9f
yonatan afewerk <yonatanafewerk@gmail.com>
2023-11-04 20:31:05 +0300

    Completed 1st round of the card components development

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import "./App.css";
 import Footer from "./component/Footer";
+import Cards from "./component/Cards";
 
 function App() {
     return (
-        <>
-            <>
+        <div className="container">
+            <Cards />
+            <div>
                 <p>In production ...</p>
                 <h1>FLO</h1>
                 <h1 className="stylish">STATE</h1>
-            </>
+            </div>
             <Footer />
-        </>
+        </div>
     );
 }
 
