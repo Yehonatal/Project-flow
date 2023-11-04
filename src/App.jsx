e1d4ebdac167adbb9c8b863f452714c800b0e7c2
yonatan afewerk <yonatanafewerk@gmail.com>
2023-11-04 13:46:09 +0300

    Development: in production

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 function App() {
     return (
         <>
+            <p>In production ...</p>
             <h1>FLO</h1>
             <h1 className="stylish">STATE</h1>
         </>
