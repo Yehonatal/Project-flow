6c56af648613f3ba307644385109586e7e9b5874
yonatan afewerk <yonatanafewerk@gmail.com>
2023-11-04 13:40:09 +0300

    Project: name changed to Flo

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
     return (
         <>
-            <h1>FLOW</h1>
+            <h1>FLO</h1>
             <h1 className="stylish">STATE</h1>
         </>
     );
