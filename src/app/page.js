eca163da72ffff96af68edc77ce53bef7786e2e3
yonatan afewerk <yonatanafewerk@gmail.com>
2024-02-07 18:26:42 +0300

    Stack change: from now on the tech stack used will be Next 😇

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,11 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div>
-                <h1 className="font-extrabold uppercase">In development ...</h1>
+                <h1 className="stylish_bold">FLO</h1>
+                <h1 className="font-extrabold uppercase opacity-45">
+                    In development ...
+                </h1>
+                <h1 className="stylish_accent">STATE</h1>
             </div>
         </main>
     );
