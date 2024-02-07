760f8c67f7a24400887b1aeed36933c5f9986811
yonatan afewerk <yonatanafewerk@gmail.com>
2024-02-07 18:13:25 +0300

    Stack change: from now on the tech stack used will be Next 😇

---
+++ src/app/layout.js
@@ -0,0 +1,18 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+    title: "F.L.O.W",
+    description:
+        "A collection of ambient sounds for when the work your doing needs that bit of touch to make it enjoyable.",
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
+}
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>
+                <h1 className="font-extrabold uppercase">In development ...</h1>
+            </div>
+        </main>
+    );
+}
